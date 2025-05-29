2756dad76cb3c8b1a6310d597b17bc933791e914
regular_man_14 <omkarchowdhury14@gmail.com>
2025-05-29 08:18:18 +0530

    added functionality to validate entered details, check if user already exists, check if files exist on the local server, upload files on cloudinary, check if they have been uploaded , and check if a new user has been successfully created

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,69 @@
 import { asyncHandler } from "../utils/asynchandler.js";
+import {ApiError} from '../utils/ApiError.js'
+import {ApiResponse} from '../utils/ApiResponse.js'
+import {User} from '../models/users.models.js'
+import { uploadOnCloudinary } from "../utils/uploads.cloudinary.utils.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+    const {fullName, email, userName, password} = req.body;
+    
+    //Check if the entered details are correct.
+    if (
+        [fullName, email, userName, password].some(
+            (field) => {
+                return field?.trim() === ""
+            })
+    ) {
+        throw new ApiError(400, "All these are required")
+    }
+
+    //Check if the user already exists
+    const userExists = User.findOne({
+        $or: [{userName}, {email}]
     })
+    
+    if (!userExists) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    
+    
+    //Check if files: avatar and coverImage exist
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")        
+    }
+    
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    
+    //Upload on cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    //And check if its present on the cloud
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        userName: userName.toLowerCase()
+    })
+
+    //Check if user exists
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Oops! Something went wrong while registering the user :(")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "New user registered successfully")
+    )
 })
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
-
+import { upload } from "../middlewares/multer.middlewares.js";
 const router = Router()
 
-router.route("/register").post(registerUser)
-
+router.route("/register").post(
+    //Bro this is a middleware that simply adds two additional fields to
+    //res.body. Ok Mumi? Don't be scared ;) 
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 export default router
