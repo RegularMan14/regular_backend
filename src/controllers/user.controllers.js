dad1cf94d10c5d959128ace823e8b3df56486748
regular_man_14 <omkarchowdhury14@gmail.com>
2025-06-12 09:51:10 +0530

    Add getUserChannelProfile endpoint and update routes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -200,7 +200,7 @@ const logoutUser = asyncHandler( async (req, res) => {
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(
-        new ApiResponse(200, { user: req.user}, "User logged out")
+        new ApiResponse(200, { user: req.user }, "User logged out")
     )
 })
 
@@ -392,6 +392,77 @@ const updateUserCoverImage = asyncHandler( async(req, res) => {
     )
 })
 
+const getUserChannelProfile = asyncHandler( async(req, res) => {
+    const {userName} = req.params
+
+    if (!userName?.trim()) {
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                userName: userName?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField:"_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField:"_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -401,5 +472,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserCoverImage, updateUserAvatar, getUserChannelProfile } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router()
@@ -23,10 +23,13 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //Secured routes
+router.route("/get-current-user").get(getCurrentUser)
+router.route("/get-user-channel-profile").get(getUserChannelProfile)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-current-password").post(changeCurrentPassword)
-router.route("/get-current-user").get(getCurrentUser)
 router.route("/update-account-details").post(updateAccountDetails)
+router.route("/update-user-avatar").post(updateUserAvatar)
+router.route("/update-user-cover-image").post(updateUserCoverImage)
 
 export default router
