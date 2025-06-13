40ad75e9a0870e4c647ebf8a0eccf7b9003599f7
regular_man_14 <omkarchowdhury14@gmail.com>
2025-06-13 08:55:15 +0530

    Enhance getWatchHistory endpoint to include owner details in the response

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -494,12 +494,29 @@ const getWatchHistory = asyncHandler ( async (req, res) => {
                                     }
                                 }
                             ]
+                        },
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
                         }
                     }
                 ]
             }
         }
     ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
 })
 
 export {
